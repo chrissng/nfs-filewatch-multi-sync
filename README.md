fa317ce60c4b5c17c955ce4a53a7e3bcf3b3e3a4
Nguyen Duc Hoa <duchoa.nguyen@starhub.com>
2015-03-27 15:28:26 +0800

    Release version 0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # nfs-filewatch-multi-sync
-A tool for watching for new files in a NFS and sync them to multiple destinations, due to the limitation of inotify in NFS
+A tool for watching for new files non-recursive in a list of folders a NFS and
+sync them to multiple destinations. Inotify has limitations with NFS.
+
+# Available switches
+
+```
+  --help|-h                           Print this help message.
+  --dormant-threshold                 Time to consider file to be dormant aka
+                                      not modified for x minute ago.
+                                      Default 2 minutes
+  --delete-source-file                Delete source file after complete.
+                                      Default is 1
+  --parallel-jobs                     Number of parallel jobs
+  --watch-folder-fromfile             Path to file contains folders to watch
+  --dest-folder-fromfile              Path to file of contains folders to output
+  --ignore-hidden-and-partial-file    Ignore (.) hidden files (rsync partial)
+                                      and .part file (ftp partial).
+                                      Default is 1
+  --polling-interval                  Interval between each poll.
+                                      Default is 30 seconds
+```
+# Sample:
+```
+./nfs-filewatch-multi-sync.sh --dormant-threshold 2 --parallel-jobs 3 \
+    --polling-interval 30 \
+    --watch-folder-fromfile ./sample/watch_folder_fromfile.txt.sample \
+    --dest-folder-fromfile ./sample/dest_folder_fromfile.txt.sample
+```
+
+Given the following contents:
+```
+# Watch folders file content
+/tmp/watcher/folder_1
+/tmp/watcher/folder_2
+
+# Dest folders file content
+/tmp/dest
+/tmp/dest2
+```
+
+Any new files being added in `folder_1` and `folder_2` will be rsynced over
+to `dest` and `dest2` if the file is not modified for 2 minutes. Say, if a
+file name `kitten.txt` stayed dormant for 2 minutes in `/tmp/watcher/folder_1`,
+after the job ran, the dest folders will contains `/tmp/dest/folder_1/kitten.txt`
+and `/tmp/dest2/folder_1/kitten.txt`
+
+# Dependencies:
+* rsync
+* parallel
+
+# Licenses:
+* MIT
---
+++ nfs-filewatch-multi-sync.sh
@@ -0,0 +1,109 @@
+#!/usr/bin/env bash
+
+VERSION=0.1.0
+DORMANT_THRESHOLD=2
+DELETE_SOURCE_FILE=1
+IGNORE_HIDDEN_AND_PARTIAL=1
+PARALLEL_JOBS=5
+POLLING_INTERVAL=30
+
+function print_help {
+    echo "You need to supply all the arguments"
+    echo "  --help|-h                Print this help message."
+    echo "  --dormant-threshold      Time to consider file to be dormant / not modified for x minute ago. Default 2 minutes"
+    echo "  --delete-source-file     Delete source file after complete. Default is 1"
+    echo "  --parallel-jobs          Number of parallel jobs"
+    echo "  --watch-folder-fromfile  Path to file contains folders to watch"
+    echo "  --dest-folder-fromfile   Path to file of contains folders to output"
+    echo "  --ignore-hidden-and-partial-file    Ignore (.) hidden files (rsync partial) and .part file (ftp partial). Default is 1"
+    echo "  --polling-interval       Interval between each poll. Default 30 seconds"
+}
+
+if [ $# -eq 0 ]; then
+    print_help
+    exit 0
+fi
+
+while [[ $# -ge 1 ]]
+do
+    KEY="$1"
+    shift
+
+    case $KEY in
+        -h|--help)
+            print_help
+            exit 0
+            ;;
+        --parallel-jobs)
+            PARALLEL_JOBS=$1
+            shift
+            ;;
+        --polling-interval)
+            POLLING_INTERVAL=$1
+            shift
+            ;;
+        --dormant-threshold)
+            DORMANT_THRESHOLD=$1
+            shift
+            ;;
+        --delete-source-file)
+            DELETE_SOURCE_FILE=$1
+            shift
+            ;;
+        --watch-folder-fromfile)
+            WATCH_FOLDER_FROMFILE=$1
+            [ -f "${WATCH_FOLDER_FROMFILE}" ] || { echo "Cannot find $1"; exit 1; }
+            shift
+            ;;
+        --dest-folder-fromfile)
+            DEST_FOLDER_FROMFILE=$1
+            [ -f "${DEST_FOLDER_FROMFILE}" ] || { echo "Cannot find $1"; exit 1; }
+            shift
+            ;;
+        --ignore-hidden-and-partial-file)
+            IGNORE_HIDDEN_AND_PARTIAL=$1
+            shift
+            ;;
+        *)
+            # unknown option
+            ;;
+    esac
+done
+
+
+if [ $IGNORE_HIDDEN_AND_PARTIAL -eq 0 ] ; then
+    FIND_IGNORE_PATTERN=''
+else
+    FIND_IGNORE_PATTERN="\( ! -regex '.*/\..*' -not -name '*.part' \)"
+fi
+
+
+WATCH_FOLDERS=$(cat $WATCH_FOLDER_FROMFILE | tr "\n" " ")
+DEST_FOLDERS=$(cat $DEST_FOLDER_FROMFILE | tr "\n" " ")
+
+while true; do
+    echo "[INFO] - Polling for changes"
+
+    FEED_FILES=$(echo $FIND_IGNORE_PATTERN | xargs find ${WATCH_FOLDERS[@]} -maxdepth 1 -type f -mmin +${DORMANT_THRESHOLD})
+
+    for FILE in ${FEED_FILES[@]}
+    do
+        echo "[INFO] - $FILE is syncing..."
+        SOURCE_FILE=`basename $FILE`
+        SOURCE_DIR=`dirname $FILE`
+        SOURCE_FOLDER=`basename $SOURCE_DIR`
+        parallel --jobs $PARALLEL_JOBS --halt 2 rsync -a --include="${SOURCE_FILE}" --exclude="*" ${SOURCE_DIR}/ {}/.tmp/${SOURCE_FOLDER} ::: ${DEST_FOLDERS[@]}
+        RSYNC_RESULT=$?
+        if [ $RSYNC_RESULT -eq 0 ]; then
+            parallel --jobs $PARALLEL_JOBS mkdir -p {}/${SOURCE_FOLDER}\; mv {}/.tmp/${SOURCE_FOLDER}/${SOURCE_FILE} {}/${SOURCE_FOLDER} ::: ${DEST_FOLDERS[@]}
+            if [ $DELETE_SOURCE_FILE -eq 1 ]; then
+                rm -f $FILE
+            fi
+        else
+            echo "[ERROR] - Failed to rsync $FILE_CHANGE!"
+        fi
+        echo "[INFO] - $FILE synced"
+    done
+
+    sleep $POLLING_INTERVAL
+done
